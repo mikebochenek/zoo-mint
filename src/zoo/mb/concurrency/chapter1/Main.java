d739691480e01c58100b6018513897ae9d9ad7e6
mikebochenek <mike.bochenek@gmail.com>
2013-09-11 21:56:53 +0200

    nanoseconds - System.nanoTime()

--- src/zoo/mb/concurrency/chapter1/Main.java
+++ src/zoo/mb/concurrency/chapter1/Main.java
@@ -9,10 +9,15 @@ package zoo.mb.concurrency.chapter1;
  */
 public class Main {
 	public static void main(String[] args) {
-		for (int i = 1; i <= 15; i++) {
+		long start = System.nanoTime();
+		for (int i = 1; i <= 150; i++) {
 			Calculator calculator = new Calculator(i);
 			Thread thread = new Thread(calculator);
 			thread.start();
 		}
+		
+		// A nanosecond (ns) is an SI unit of time equal to one billionth of a second (10−9 or 1/1,000,000,000 s). 
+		System.out.println("\n\n   nano elapsed: " + (System.nanoTime() - start));
+		System.out.println("\n\n   nano elapsed: " + ((System.nanoTime() - start) / 1000000L));
 	}
 }
