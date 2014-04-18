8a0bce8ed29b912c75a26d60da99d35138749e0a
mikebochenek <mike.bochenek@gmail.com>
2014-04-18 13:59:08 +0200

    first buggy implementation

--- src/zoo/mb/keypad/Keypad.java
+++ src/zoo/mb/keypad/Keypad.java
@@ -1,5 +1,8 @@
 package zoo.mb.keypad;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * http://www.dialabc.com/words/history.html
  * 
@@ -9,13 +12,42 @@ package zoo.mb.keypad;
  *  5 JKL     9 WXYZ   
  */
 public class Keypad {
+	
+	final char[][] pad = { {'0'}, // 0
+			{'1'}, // 1
+			{'A','B','C'}, // 2
+			{'D','E','F'}, // 3
+			{'G','H','I'}, // 4
+			{'J','K','L'}, // 5
+			{'M','N','O'}, // 6
+			{'P','Q','R','S'}, // 7
+			{'T','U','V'}, // 8
+			{'W','X','Y','Z'} }; // 9
 
 	public long getNumber(String word) {
 		return -1;
 	}
 	
-	public String[] getAllCombinations(long num) {
-		return null;
+	public Set<String> getAllCombinations(long num) {
+		Set<String> all = new HashSet<String>();
+		
+		//assert num > 100; // what about numbers starting with zero?
+		
+		String numStr = "" + num;
+		
+		char[] result = numStr.toCharArray(); //new char[numStr.length()];
+		
+		for (int i = 0; i < numStr.length(); i++) {
+			int idx = (int)numStr.charAt(i);
+			idx = idx - 48; //TODO some asserts or checks
+			
+			for (int j = 0; j < pad[idx].length; j++) {
+				result[i] = pad[idx][j];
+				all.add(new String(result));
+			}
+		}
+		
+		return all;
 	}
 	
 }
--- src/zoo/mb/keypad/KeypadTest.java
+++ src/zoo/mb/keypad/KeypadTest.java
@@ -2,6 +2,8 @@ package zoo.mb.keypad;
 
 import static org.junit.Assert.*;
 
+import java.util.Set;
+
 import org.junit.Test;
 
 public class KeypadTest {
@@ -13,7 +15,13 @@ public class KeypadTest {
 
 	@Test
 	public void testGetAllCombinations() {
-		fail("Not yet implemented");
+		Keypad pad = new Keypad();
+		Set<String> all = pad.getAllCombinations(2553249);
+		for (String s : all) {
+			System.out.println(s);
+		}
+		
+		System.out.println(all.size() + " in total.");
 	}
 
 }
