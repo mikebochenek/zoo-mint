72348252953cbdb5547d8acc41c0e5bb39ba0170
mike.bochenek@gmail.com <mike.bochenek@gmail.com>
2015-12-26 21:18:28 +0100

    tiny little tweeks

--- src/zoo/mb/concurrency/chapter1/RandomWinton.java
+++ src/zoo/mb/concurrency/chapter1/RandomWinton.java
@@ -2,19 +2,33 @@ package zoo.mb.concurrency.chapter1;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.text.DecimalFormat;
+import java.util.List;
 import java.util.Random;
 
 public class RandomWinton {
 
 	public static void main(String[] args) {
 		long startTS = System.currentTimeMillis();
+		
+		try {
+			List<String> lines = Files.readAllLines(Paths.get("/home/mike/data/winton/train1000.csv"), Charset.forName("UTF-8"));
+			for(String line:lines){
+				  System.out.println(line);
+				}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 		try {
 			PrintWriter writer = new PrintWriter("/tmp/submission-" + System.currentTimeMillis() + ".csv", "UTF-8");
 			writer.println("Id,Predicted");
 			for (int i = 0; i < 60000; i++) {
 				for (int j = 0; j < 62; j++) {
-					writer.println((i+1) + "_" + (j+1) + "," + randomReturn(0.0000004));
+					writer.println((i+1) + "_" + (j+1) + "," + randomReturn(0.000000004));
 				}
 				//writer.println((i+1) + "_" + (61) + "," + randomReturn(0.004)); somehow this made things worse actually....
 				//writer.println((i+1) + "_" + (62) + "," + randomReturn(0.004));
@@ -28,7 +42,7 @@ public class RandomWinton {
 	}
 	
 	private static Random random = new Random();
-	private static DecimalFormat format = new DecimalFormat("#.########");
+	private static DecimalFormat format = new DecimalFormat("#.###########");
 	
 	public static String randomReturn(double sizer) {
 		double r = random.nextDouble();
